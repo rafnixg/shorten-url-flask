f9241231262eff10edadc0cf1d8218cc5f4190c2
Rafnixg <rafnix.guzman@sead.pe>
2019-04-29 17:15:02 -0500

    [ADD] - Link model and Schema

--- app.py
+++ app.py
@@ -14,6 +14,26 @@ db = SQLAlchemy(app)
 # Init ma
 ma = Marshmallow(app)
 
+# Link Model
+class Link(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(200))
+    hash = db.Column(db.String(6),unique=True)
+    hint = db.Column(db.Integer)
+
+    def __init__(self,url,hash,hint):
+        self.url = url
+        self.hash = hash
+        self.hint = hint
+
+# Link Schema
+class LinkSchema(ma.Schema):
+    class Meta:
+        fields = ('id','url','hash','hint')
+
+link_schema = LinkSchema(strict=True)
+links_schema = LinkSchema(many=True, strict=True)
+
 
 # Run Server
 if __name__ == '__main__':
