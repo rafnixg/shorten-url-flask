0c106f4c99aefa51583d0d6eb7999336c46df1f3
Rafnix Guzmán <rafnixg@gmail.com>
2021-09-20 19:39:44 -0500

    [REF] Flask v2

--- README.md
+++ README.md
@@ -1,15 +1,33 @@
 # URL Shortener in Flask
 Web service using Flask framework for Shortener URLs
 
+## Install
+Create Virtual env
+```bash
+$ python3 -m venv env
+```
+Install requirements.txt
+```bash
+$ source env/bin/activate
+$ pip install -r requirements.txt
+```
+Initializate the DB
+```bash
+$ flask shell
+>>> from app import db
+>>> db.create_all()
+>>> exit()
+```
+## Run
+```bash
+$ flask run
+```
 ## Testing using CURL
-
-### Create new link
-
+Create new link
 ```bash 
 $ curl --header "Content-Type: application/json" --request POST --data '{"url":"http://github.com/"}' http://localhost:5000/
 ```
-
-### Get all links
+Get all links
 
 ```bash
 $ curl --header "Content-Type: application/json" --request GET http://localhost:5000/
--- app.py
+++ app.py
@@ -1,68 +1,79 @@
+import os, hashlib, base64
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-import os, hashlib, base64
+# from hashids import Hashids
+
 
 # Init App
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 # Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
+    basedir, "db.sqlite"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# app.config['SECRET_KEY'] = 'supersecret'
 # Init db
 db = SQLAlchemy(app)
 # Init ma
 ma = Marshmallow(app)
+# Init Hashids
+# hashids = Hashids(min_length=4, salt=app.config['SECRET_KEY'])
 
 # Link Model
 class Link(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     url = db.Column(db.String(200))
-    hash = db.Column(db.String(6),unique=True)
+    hash = db.Column(db.String(6), unique=True)
     hint = db.Column(db.Integer)
 
-    def __init__(self,url,hash,hint):
+    def __init__(self, url, hash, hint):
         self.url = url
         self.hash = hash
         self.hint = hint
 
+
 # Link Schema
 class LinkSchema(ma.Schema):
     class Meta:
-        fields = ('id','url','hash','hint')
+        fields = ("id", "url", "hash", "hint")
 
-link_schema = LinkSchema(strict=True)
-links_schema = LinkSchema(many=True, strict=True)
+
+link_schema = LinkSchema()
+links_schema = LinkSchema(many=True)
 
 # Routes
 
 # Create a Link
-@app.route('/',methods=['POST'])
+@app.route("/", methods=["POST"])
 def create_link():
-    url = request.json['url']
+    url = request.json["url"]
     # Hash from url
-    hash = base64.urlsafe_b64encode(hashlib.md5(url).digest())[:6]
+    hash = base64.urlsafe_b64encode(hashlib.md5(url.encode('utf-8')).digest())[:6]
     hint = 0
     # Validate hash (Unique)
-    if(Link.query.filter(Link.hash == hash).first()):
-        return jsonify({'hash':hash})
+    if Link.query.filter(Link.hash == hash).first():
+        return jsonify({"hash": hash})
     else:
-        new_link = Link(url,hash,hint)
+        new_link = Link(url, hash, hint)
 
         db.session.add(new_link)
         db.session.commit()
 
         return link_schema.jsonify(new_link)
 
+
 # Get all Links
-@app.route('/',methods=['GET'])
+@app.route("/", methods=["GET"])
 def get_links():
     links = Link.query.all()
     res = links_schema.dump(links)
     return jsonify(res.data)
 
+
 # Get link
-@app.route('/<hash>',methods=['GET'])
+@app.route("/<hash>", methods=["GET"])
 def get_link(hash):
     link = Link.query.filter(Link.hash == hash).first()
     if link:
@@ -70,6 +81,7 @@ def get_link(hash):
         db.session.commit()
     return link_schema.jsonify(link)
 
+
 # Run Server
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
